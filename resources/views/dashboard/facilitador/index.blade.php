ef768b96aaa05d4cdc0e1f4d61e81e8bd038f635
Rubens Flinco <rubens@planetsweb.com.br>
2019-10-12 18:24:58 -0300

    page home facilitador

--- resources/views/dashboard/facilitador/index.blade.php
+++ resources/views/dashboard/facilitador/index.blade.php
@@ -3,36 +3,44 @@
 @section('conteudo')
 
 <div class="separacao-transparente">
-        <div class="row">
-            <div class="container">
+  <div class="row">
+    <div class="container">
+      <h3 class="text-center">Olá {{@Auth::user()->name}}, tudo bem ?</h3>
+      <hr/><br/>
 
-                    <center>
-                            <h3>Olá {{@Auth::user()->name}}, tudo bem ?</h3>
-                            <br/>
-                        </center>
-                      
-                  <div class="panel">
-                    <div class="panel-heading">
-                      <div class="container">
-                        <h4><b>
-                            <i class="fa fa-users" aria-hidden="true"></i>
-                            Turmas
-                          </b></h4>
-                        <center>
-                          <hr>
-                          <p>Você tem: {NUMERO_DE_TURMAS}</p>
-                        </center>
-                      </div>
-                    </div>
-                  </div>
+      <div class="row">
 
-
-                  <a href="{{ route('instituicao.cadastrar') }}" >
-                    Cad Inst
-                  </a>
-                      
-    
-            </div>
+        <div class="col-md-3 col-xs-6">
         </div>
+
+        <div class="col-md-3 col-xs-6">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <i class="fa fa-users" aria-hidden="true"></i> Participantes:
+            </div>
+            <div class="panel-body">
+                {Numero}
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3 col-xs-6">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <i class="fa fa-bullhorn" aria-hidden="true"></i> Campanhas:
+            </div>
+            <div class="panel-body">
+                {Numero}
+            </div>
+          </div>
+        </div>
+        
+      </div>
+
+      <hr/><br/>
+
+      
+    </div>
+  </div>
 </div>
 @endsection
