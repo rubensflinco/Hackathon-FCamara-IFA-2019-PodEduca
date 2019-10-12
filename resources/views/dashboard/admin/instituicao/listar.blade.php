65e943ea1e2d08163fc699517e29f5ba2719fab2
Rubens Flinco <rubens@planetsweb.com.br>
2019-10-12 19:18:25 -0300

    pagina instituição lista

--- resources/views/dashboard/admin/instituicao/listar.blade.php
+++ resources/views/dashboard/admin/instituicao/listar.blade.php
@@ -1,5 +1,64 @@
 @extends('layouts.default')
 @section('menu', true)
+@section('dashboard', true)
+@section('conteudo')
+
+<div class="separacao-transparente">
+  <div class="row">
+    <div class="container">
+      <ol class="breadcrumb fundo-transparente">
+          <li><a href="/">Home</a></li>
+          <li class="active"><a href="#">Instituições</a></li>
+          
+          <a class="btn btn-warning btn-md pull-right" href="/dashboard/instituicao/cadastrar">Cadastrar instituição</a>
+          <br/><br/>
+      </ol>
+
+      <table class="table table-striped">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Nome</th>
+              <th>Estado</th>
+              <th class="text-center">Opções</th>
+            </tr>
+          </thead>
+          @foreach ($instituicoes as $instituicao)
+          <tbody>
+            <tr>
+              <th scope="row">{{ $instituicao->id }}</th>
+              <td>{{ $instituicao->nome }}</td>
+              <td>{{ $instituicao->localidade }}</td>
+              <td class="text-center">
+
+              <form style="display: inline" method="post" action="/dashboard/instituicao/editar/{{ $instituicao->id }}">
+                @csrf
+                @method('GET')
+                  <button class="btn btn-warning btn-circle"><i class="fa fa-pencil" aria-hidden="true"></i></button>
+              </form>
+                
+
+              <form style="display: inline" method="post" action="/dashboard/instituicao/remover/{{ $instituicao->id }}"
+                onsubmit="return confirm('Tem certeza que deseja remover {{addslashes($instituicao->nome)}}')">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-danger btn-circle"><i class="fa fa-times" aria-hidden="true"></i></button>
+              </form>
+
+              </td>
+            </tr>
+          </tbody>
+          @endforeach
+        </table>
+      
+    </div>
+  </div>
+</div>
+@endsection
+
+
+{{-- @extends('layouts.default')
+@section('menu', true)
 @section('conteudo')
 
 <div class="separacao-transparente">
@@ -51,4 +110,4 @@
             </div>
         </div>
 </div>
-@endsection
+@endsection --}}
