c922bca7b46f0d7f75db7f2909de8a1e13dc31f7
Rubens Flinco <rubens@planetsweb.com.br>
2019-10-12 16:53:05 -0300

    nova navbar

--- resources/views/layouts/menu/index.blade.php
+++ resources/views/layouts/menu/index.blade.php
@@ -4,35 +4,23 @@
   @section('conteudo.menu.lateral')
   @switch(Auth::user()->grupo)
       @case("admin")
-          <a href="/dashboard">
-            <i class="fa fa-home " aria-hidden="true"></i><span>Usuário</span>
+          <a href="/dashboard/poderes">
+            <i class="fa fa-fire " aria-hidden="true"></i><span>Poderes</span>
           </a>
-          <a href="/dashboard/turmas">
-            <i class="fa fa-home " aria-hidden="true"></i><span>Turmas</span>
+          <a href="/dashboard/missoes">
+            <i class="fa fa-flag-checkered " aria-hidden="true"></i><span>Missões</span>
           </a>
-          <a href="/dashboard">
-            <i class="fa fa-home " aria-hidden="true"></i><span>Conta</span>
+          <a href="/dashboard/facilitadores">
+            <i class="fa fa-users " aria-hidden="true"></i><span>Facilitadores</span>
           </a>
-          <a href="/dashboard">
-            <i class="fa fa-home " aria-hidden="true"></i><span>Missão</span>
-          </a>
-          <a href="/dashboard">
-            <i class="fa fa-home " aria-hidden="true"></i><span>Poderes</span>
-          </a>
-          <a href="/dashboard">
-            <i class="fa fa-home " aria-hidden="true"></i><span>Instituições</span>
-          </a>
-          <a href="/dashboard">
-            <i class="fa fa-home " aria-hidden="true"></i><span>Oficinas</span>
+          <a href="/dashboard/instituicoes">
+            <i class="fa fa-building-o " aria-hidden="true"></i><span>Instituições</span>
           </a>
           @break
 
       @case("facilitador")
-          <a href="/dashboard/turmas">
-            <i class="fa fa-users " aria-hidden="true"></i><span>Campanha</span>
-          </a>
-          <a href="/dashboard/turmas">
-            <i class="fa fa-users " aria-hidden="true"></i><span>Encontro</span>
+          <a href="/dashboard/campanha">
+            <i class="fa fa-bullhorn " aria-hidden="true"></i><span>Campanha</span>
           </a>
           <a href="/dashboard/participante/listar">
             <i class="fa fa-users " aria-hidden="true"></i><span>Participantes</span>
@@ -40,17 +28,11 @@
           @break
       
       @case("participante")
-          <a href="/dashboard/turma">
-            <i class="fa fa-users " aria-hidden="true"></i><span>Turma</span>
-          </a>
           <a href="/dashboard/missoes">
             <i class="fa fa-flag-checkered " aria-hidden="true"></i><span>Missões</span>
           </a>
           <a href="/dashboard/poderes">
             <i class="fa fa-fire " aria-hidden="true"></i><span>Poderes</span>
-          </a>
-          <a href="/dashboard/conta">
-            <i class="fa fa-user " aria-hidden="true"></i><span>Conta</span>
           </a>
           @break
           
@@ -66,9 +48,30 @@
 
       @extends('layouts.menu.mobile.baixo')
       @section('conteudo.menu.mobile.baixo')
-        <li><a href="/dashboard/conta">
-            <i class="button fa fa-user fa-2x fa-mobile-3x" aria-hidden="true"></i><p>Conta</p>
-        </a></li>
+
+       @switch(Auth::user()->grupo)
+      @case("admin")
+          <li><a href="/dashboard/instituicoes">
+              <i class="button fa fa-building-o fa-2x fa-mobile-3x" aria-hidden="true"></i><p>Instituições</p>
+          </a></li>
+          @break
+
+      @case("facilitador")
+          <li><a href="/dashboard/campanha">
+              <i class="button fa fa-bullhorn fa-2x fa-mobile-3x" aria-hidden="true"></i><p>Campanha</p>
+          </a></li>
+          @break
+      
+      @case("participante")
+          <li><a href="/dashboard/missoes">
+              <i class="button fa fa-flag-checkered fa-2x fa-mobile-3x" aria-hidden="true"></i><p>Missões</p>
+          </a></li>
+          @break
+          
+      @default
+          
+  @endswitch
+
       @endsection
 
     </div>
