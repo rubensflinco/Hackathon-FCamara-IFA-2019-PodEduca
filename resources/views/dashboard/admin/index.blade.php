fd31158863f4a9e4fc388080be3ab3860dce67ca
Rubens Flinco <rubens@planetsweb.com.br>
2019-10-12 18:20:37 -0300

    home do admin

--- resources/views/dashboard/admin/index.blade.php
+++ resources/views/dashboard/admin/index.blade.php
@@ -3,14 +3,63 @@
 @section('conteudo')
 
 <div class="separacao-transparente">
-        <div class="row">
-            <div class="container">
+  <div class="row">
+    <div class="container">
+      <h3 class="text-center">Olá {{@Auth::user()->name}}, tudo bem ?</h3>
+      <hr/><br/>
 
-                <h3>Admin</h3>
-
-                <p>Olá {{@Auth::user()->name}}, seja bem vindo!!!</p>
-
+      <div class="row">
+          
+        <div class="col-md-3 col-xs-6">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <i class="fa fa-building-o" aria-hidden="true"></i> Intituições:
             </div>
+            <div class="panel-body">
+                {Numero}
+            </div>
+          </div>
         </div>
+
+        <div class="col-md-3 col-xs-6">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <i class="fa fa-users" aria-hidden="true"></i> Participantes:
+            </div>
+            <div class="panel-body">
+                {Numero}
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3 col-xs-6">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <i class="fa fa-users" aria-hidden="true"></i> Facilitadores:
+            </div>
+            <div class="panel-body">
+                {Numero}
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3 col-xs-6">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <i class="fa fa-bullhorn" aria-hidden="true"></i> Campanhas:
+            </div>
+            <div class="panel-body">
+                {Numero}
+            </div>
+          </div>
+        </div>
+        
+      </div>
+
+      <hr/><br/>
+
+      
+    </div>
+  </div>
 </div>
 @endsection
