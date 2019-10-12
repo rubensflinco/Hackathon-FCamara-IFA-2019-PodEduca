b3b6ef98201059642b9ee07b3406e8cc58715a86
Rubens Flinco <rubens@planetsweb.com.br>
2019-10-12 09:11:46 -0300

    facilitador

--- resources/views/dashboard/facilitador/index.blade.php
+++ resources/views/dashboard/facilitador/index.blade.php
@@ -11,20 +11,25 @@
                             <br/>
                         </center>
                       
-                              <div class="panel">
-                                <div class="panel-heading">
-                                  <div class="container">
-                                    <h4><b>
-                                        <i class="fa fa-users" aria-hidden="true"></i>
-                                        Turmas
-                                      </b></h4>
-                                    <center>
-                                      <hr>
-                                      <p>Você tem: {NUMERO_DE_TURMAS}</p>
-                                    </center>
-                                  </div>
-                                </div>
-                              </div>
+                  <div class="panel">
+                    <div class="panel-heading">
+                      <div class="container">
+                        <h4><b>
+                            <i class="fa fa-users" aria-hidden="true"></i>
+                            Turmas
+                          </b></h4>
+                        <center>
+                          <hr>
+                          <p>Você tem: {NUMERO_DE_TURMAS}</p>
+                        </center>
+                      </div>
+                    </div>
+                  </div>
+
+
+                  <a href="{{ route('instituicao.cadastrar') }}" >
+                    Cad Inst
+                  </a>
                       
     
             </div>
