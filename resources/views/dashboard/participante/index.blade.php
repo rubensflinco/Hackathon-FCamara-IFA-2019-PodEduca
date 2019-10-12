76fe56f6da041ffd007abe261fbcd985b24e5385
Rubens Flinco <rubens@planetsweb.com.br>
2019-10-12 18:15:24 -0300

    home participantes

--- resources/views/dashboard/participante/index.blade.php
+++ resources/views/dashboard/participante/index.blade.php
@@ -3,67 +3,83 @@
 @section('conteudo')
 
 <div class="separacao-transparente">
-        <div class="row">
-            <div class="container">
-                  <div class="perfil">
-                    <img src="https://zdnet4.cbsistatic.com/hub/i/r/2017/03/06/a4a5bb7b-7a1d-4f2d-8be2-a78455e8f749/resize/1200x900/9584580a6d94305113961f57d4421745/github-logo-silhouette-in-a-square318-54633.jpg" class="perfil" alt="img_perfil">
-                    <div>
-                      <h3>Olá {{@Auth::user()->name}}, tudo bem ?</h3> 
-                      <p>Veja aqui suas ...</p>
-                           
-                    </div>  
-                  </div>
-                  
-                    <div class="painel">
-                      <h3>Acompanhe seu dados:</h3>
-                      <div>
-                        <p><b>Seus Pontos:</b> {Numero de pontos}</p>
-                        <p><b>A sua posição no hanking da turma:</b> {Numero da posição}</p>
-                        <p><b>Os seus poderes:</b></p>
-                        <div class="container" style="display: flex">
-                            <div class="card">
-                              <img class="card-img-top" src="https://static6.depositphotos.com/1025854/540/v/950/depositphotos_5404706-stock-illustration-human-brain-comics-drawing.jpg" width="100%" alt="Imagem de capa do card">
-                              <div class="card-body">
-                                <p class="card-text">Inteligência</p>
-                              </div>
-                            </div>
-                            <div class="card hidden-xs">
-                              <img class="card-img-top" src="https://static6.depositphotos.com/1025854/540/v/950/depositphotos_5404706-stock-illustration-human-brain-comics-drawing.jpg" width="100%" alt="Imagem de capa do card">
-                              <div class="card-body">
-                                <p class="card-text">Inteligência</p>
-                              </div>
-                            </div>
-                            <div class="card hidden-xs">
-                              <img class="card-img-top" src="https://static6.depositphotos.com/1025854/540/v/950/depositphotos_5404706-stock-illustration-human-brain-comics-drawing.jpg" width="100%" alt="Imagem de capa do card">
-                              <div class="card-body">
-                                <p class="card-text">Inteligência</p>
-                              </div>
-                            </div> 
-                            <div class="card hidden-xs">
-                              <img class="card-img-top" src="https://static6.depositphotos.com/1025854/540/v/950/depositphotos_5404706-stock-illustration-human-brain-comics-drawing.jpg" width="100%" alt="Imagem de capa do card">
-                              <div class="card-body">
-                                <p class="card-text">Inteligência</p>
-                              </div>
-                            </div>                  
-                        </div>
-                      </div>
-                    </div>
-                    <!-- <div class="panel">
-                            <div class="panel-heading">
-                              <div class="container">
-                                <h4><b>
-                                    <i class="fa fa-level-up" aria-hidden="true"></i>
-                                    Rank
-                                  </b></h4>
-                                <center>
-                                  <hr>
-                                  <p>Seu rank atual na sua turma é: 0</p>
-                                </center>
-                              </div>
-                            </div>
-                          </div> -->
-                
+  <div class="row">
+    <div class="container">
+      <h3 class="text-center">Olá {{@Auth::user()->name}}, tudo bem ?</h3>
+      <hr/><br/>
+
+      <div class="row">
+          
+        <div class="col-md-3 col-xs-6">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <i class="fa fa-trophy" aria-hidden="true"></i> Ranking:
             </div>
+            <div class="panel-body">
+                {Numero da posição}
+            </div>
+          </div>
         </div>
+
+        <div class="col-md-3 col-xs-6">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <i class="fa fa-gamepad" aria-hidden="true"></i> Pontos:
+            </div>
+            <div class="panel-body">
+                {Numero da posição}
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3 col-xs-6">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <i class="fa fa-flag-checkered" aria-hidden="true"></i> Missões:
+            </div>
+            <div class="panel-body">
+                {Numero da posição}
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3 col-xs-6">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <i class="fa fa-fire" aria-hidden="true"></i> Poder:
+            </div>
+            <div class="panel-body">
+                {Numero da posição}
+            </div>
+          </div>
+        </div>
+        
+      </div>
+
+      <hr/><br/>
+
+      @php $idCarousel = rand(1,1000) @endphp
+      @include('layouts.carousel.carousel', ['local' => 'top', 'id' => $idCarousel])
+
+        @include('layouts.carousel.thumbnail.thumbnail', [
+          'classItem' => 'active',
+          'titulo' => '',
+          'img' => asset('img/ifa-artes/poderes/Cards_frente-01.jpg'),
+          'descricao' => ''
+        ])
+        
+
+        @include('layouts.carousel.thumbnail.thumbnail', [
+          'classItem' => ' ',
+          'titulo' => '',
+          'img' => asset('img/ifa-artes/poderes/Cards_frente-02.jpg'),
+          'descricao' => ''
+        ])
+      
+      @include('layouts.carousel.carousel', ['local' => 'bottom', 'id' => $idCarousel])
+
+      
+    </div>
+  </div>
 </div>
 @endsection
