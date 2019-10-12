146d489d9044d55511a4c2f37af9831a18576930
João Vitor Fonseca da Silva <joaovfonseca@outlook.com>
2019-10-12 14:43:26 -0300

    Correções

---
+++ resources/views/dashboard/facilitador/campanha/criar.blade.php
@@ -0,0 +1,17 @@
+<form method="post">
+    @csrf
+    <label>Nome</label>
+    <input type="text" name="nome" id="nome">
+    <label>Nick</label>
+    <input type="text" name="nick" id="nick">
+    <label>Genero</label>
+    <select id="genero" name="genero">
+        <option value="masculino">Masculino</option>
+        <option value="ferminino">Feminino</option>
+    </select>
+        <label>Data de nascimento</label>
+        <input type="date" name="data_nascimento" id="data_nascimento">
+        <label>senha</label>
+        <input type="password" name="senha" id="senha">
+    <button type="submit">Cadastrar</button>
+</form>
