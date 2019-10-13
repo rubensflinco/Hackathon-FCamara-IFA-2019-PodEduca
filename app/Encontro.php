707169904adbe1fc5765a2110de82579d71dfb41
João Vitor Fonseca da Silva <joaovfonseca@outlook.com>
2019-10-13 08:15:11 -0300

    Model encontro

--- app/Encontro.php
+++ app/Encontro.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Encontro extends Model
 {
-    //
+    
+    protected $table = 'encontro';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id', 'facilitador_id', 'participante_id', 'campanha_id', 'poder_id', 'data', 'missao_id', 'atingiuMissao', 'atingiuMissao', 'presente'
+    ];
 }
