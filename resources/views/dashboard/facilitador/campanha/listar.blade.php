aa1631c0daf33a1b976d0b010100e297dc830655
João Vitor Fonseca da Silva <joaovfonseca@outlook.com>
2019-10-13 02:39:17 -0300

    Alterações na dashboard de facilitador

--- resources/views/dashboard/facilitador/campanha/listar.blade.php
+++ resources/views/dashboard/facilitador/campanha/listar.blade.php
@@ -66,7 +66,7 @@
       <ol class="breadcrumb fundo-transparente">
           <li><a href="/">Home</a></li>
           <li><a href="/dashboard/campanha/listar">Campanha</a></li>
-          @if ($participante->nome)
+          @if ($campanha->nome)
             <li class="active">Editar</li>
           @else
             <li class="active">Cadastrar</li>
@@ -78,16 +78,16 @@
       <div class="col-md-6 col-md-offset-3">
             <form method="POST" class="form form-black center-img">
                 @csrf
-                <input value="{{ $participante->name }}" type="text" name="name" id="name" class="form-control" placeholder="Digite o nome">
-                <input value="{{ $participante->nick }}" type="text" name="nick" id="nick" class="form-control" placeholder="Digite o nick">
-                <select value="{{ $participante->genero }}" id="genero" name="genero" class="form-control">
+                <input value="{{ $campanha->name }}" type="text" name="name" id="name" class="form-control" placeholder="Digite o nome">
+                <input value="{{ $campanha->nick }}" type="text" name="nick" id="nick" class="form-control" placeholder="Digite o nick">
+                <select value="{{ $campanha->genero }}" id="genero" name="genero" class="form-control">
                     <option value="null">Digite genero</option>
                     <option value="masculino">Masculino</option>
                     <option value="ferminino">Feminino</option>
                 </select>
                 <p class="text-left margin-dt">Data de Nascimento:</p>
-                <input value="{{ $participante->date }}" type="date" name="data_nascimento" id="data_nascimento" class="form-control">
-                <input value="{{ $participante->password }}" type="password" name="password" id="password" class="form-control" placeholder="Digite a senha">
+                <input value="{{ $campanha->date }}" type="date" name="data_nascimento" id="data_nascimento" class="form-control">
+                <input value="{{ $campanha->password }}" type="password" name="password" id="password" class="form-control" placeholder="Digite a senha">
                 <br/>
                 <button type="submit" class="btn btn-warning btn-lg">Cadastrar</button>
             </form>
