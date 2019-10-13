6e6999ec3cad1f078958ad65be74299aaa456f37
Rubens Flinco <rubens@planetsweb.com.br>
2019-10-13 07:25:25 -0300

    page perfil update

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Poder;
 use Illuminate\Http\Request;
 
 class PerfilController extends Controller
@@ -15,12 +16,15 @@ class PerfilController extends Controller
     
     public function index($id) {
         $perfil = User::find($id);
+        $poderes = Poder::all();
         if($perfil)
-            return view('dashboard.perfil', compact('perfil', $perfil));
+            return view('dashboard.perfil', compact('perfil', $perfil), compact('poderes', $poderes));
         else{
             $error = $perfil;
             return response()->view('errors', compact('error', $error) );
         }
     }
+
+
 
 }
